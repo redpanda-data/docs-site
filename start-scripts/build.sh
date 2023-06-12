6720305e7b766291346d84b6837f3d69bc73405e
JakeSCahill <jake@redpanda.com>
2023-06-12 16:47:31 +0100

    Fix build

--- start-scripts/build.sh
+++ start-scripts/build.sh
@@ -2,7 +2,7 @@
 
 # Get the latest Redpanda Console version
 export CONSOLE_VERSION=$(node start-scripts/getLatestConsoleVersion.js);
-
+echo Setting Redpanda Console version to $CONSOLE_VERSION
 # Set the latest-console-version attribute
 if [[ -n "$CONSOLE_VERSION" ]]; then
   attribute="--attribute latest-console-version=$CONSOLE_VERSION"
