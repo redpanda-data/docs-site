e1bf9cc91f79ffa2ecd7b0aaf7d0771e2fce82cc
JakeSCahill <jake@redpanda.com>
2025-05-27 22:09:44 +0100

    Fix origin var

--- netlify/edge-functions/proxy-api-docs.js
+++ netlify/edge-functions/proxy-api-docs.js
@@ -1,6 +1,6 @@
 export default async (request, context) => {
   const url = new URL(request.url);
-  // Redirects from the old API paths to the new Bump.sh ones
+  const originalOrigin = url.origin;  // Redirects from the old API paths to the new Bump.sh ones
   const redirects = {
     "/api/admin-api": "/api/doc/admin/",
     "/api/http-proxy-api": "/api/doc/http-proxy/",
@@ -19,10 +19,10 @@ export default async (request, context) => {
   }
 
   // Change target host to Bump.sh
-  url.host = "bump.sh";
+  const bumpUrl = new URL(request.url);
+  bumpUrl.host = "bump.sh";
   // Change target path to Bump.sh' Redpanda Hub
-  url.pathname = `/redpanda/hub/redpanda${url.pathname.replace('/api', '')}`;
-  const bumpUrl = url;
+  bumpUrl.pathname = `/redpanda/hub/redpanda${bumpUrl.pathname.replace('/api', '')}`;
   const secret = Netlify.env.get("BUMP_PROXY_SECRET");
 
   const bumpRes = await fetch(bumpUrl, {
@@ -46,16 +46,16 @@ export default async (request, context) => {
       footerWidget,
     ] = await Promise.all([
       bumpRes.text(),
-      fetch(`${url.origin}/_/assets/widgets/head-styles.html`).then((res) =>
+      fetch(`${originalOrigin}/_/assets/widgets/head-styles.html`).then((res) =>
         res.ok ? res.text() : ""
       ),
-      fetch(`${url.origin}/_/assets/widgets/head-script.html`).then((res) =>
+      fetch(`${originalOrigin}/_/assets/widgets/head-script.html`).then((res) =>
         res.ok ? res.text() : ""
       ),
-      fetch(`${url.origin}/_/assets/widgets/header-content.html`).then((res) =>
+      fetch(`${originalOrigin}/_/assets/widgets/header-content.html`).then((res) =>
         res.ok ? res.text() : ""
       ),
-      fetch(`${url.origin}/_/assets/widgets/footer.html`).then((res) =>
+      fetch(`${originalOrigin}/_/assets/widgets/footer.html`).then((res) =>
         res.ok ? res.text() : ""
       ),
     ]);
@@ -65,7 +65,7 @@ export default async (request, context) => {
     const modifiedHtml = originalHtml
       .replace(`<meta name="custom-head" />`, combinedHead)
       .replace(`<div id="embed-top-body" data-embed-target="top"></div>`, headerWidget)
-      .replace(`<div id="embed-bottom-body-hello"></div>`, footerWidget);
+      .replace(`<div id="embed-bottom-body"></div>`, footerWidget);
 
     return new Response(modifiedHtml, {
       status: 200,
