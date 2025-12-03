cba53fa9ab9ef8573a8eb764807fad16d838025b
JakeSCahill <jake@redpanda.com>
2025-12-03 15:58:13 +0000

    Pin the version of the deno-dom-wasm import

--- netlify/edge-functions/proxy-api-docs.js
+++ netlify/edge-functions/proxy-api-docs.js
@@ -1,4 +1,4 @@
-import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.56/deno-dom-wasm.ts";
 
 export default async (request, context) => {
   const url = new URL(request.url);
